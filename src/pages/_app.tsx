7c80923ffb07c0a3c3e5137ec197714ad0382b0d
xuy <crozzagerhc@hotmail.com>
2023-06-17 16:05:48 -0600

    ADD styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import Reset from "../styles/globalstyles"
+import * as THEME from "../styles/tudn/index";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme=THEME;
+  return (
+    <ThemeProvider theme={theme}>
+      <Reset/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
